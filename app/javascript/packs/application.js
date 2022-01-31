0a7b2989d6d836bf5fc18dcb2bfb35d18c6a716f
kazuma-soon <podradio2020@gmail.com>
2022-01-31 18:30:27 +0900

    Add: hammerjsの導入とスワイプ機能の導入#28

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 import 'bootstrap';
+import 'hammerjs';
 import '../stylesheets/application';
 
 import '@fortawesome/fontawesome-free/js/all';
@@ -12,8 +13,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
 require("src/board_image_preview");
+require("src/swipe");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
