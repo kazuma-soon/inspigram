a84d7ef0e7da60a4b3532dfc0dc2db70b4b38e6b
kazuma-soon <podradio2020@gmail.com>
2022-02-05 01:51:51 +0900

    Add: スマホウィンドウのスライドショー追加, 表示画像をいいね順に変更#47

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
   skip_before_action :check_logged_in, only: :index
 
   def index
-    # randomとlimit使うか、
-    @boards = Board.find(Board.pluck(:id).shuffle[0..5])
+    # randomとlimit使うか、人気ランキングをここで行う
+    @boards = Board.find(Reaction.where(status: 'like').group(:board_id).order('count(board_id) desc').limit(6).pluck(:board_id))
   end
 end
--- app/javascript/src/slider.js
+++ app/javascript/src/slider.js
@@ -1,8 +1,20 @@
-$(document).ready(function(){
-  $('.slider').slick({
-    centerMode: true,
-    slidesToShow: 3,
-    arrows: false,
-    autoplay: true,
-  });
+$(window).on('load resize', function(){
+  var wid = window.innerWidth;
+  if (wid >= 800) {
+    $(document).ready(function(){
+      $('.slider').slick({
+        centerMode: true,
+        slidesToShow: 3,
+        arrows: false,
+        autoplay: true,
+      });
+    });
+  } 
+  else {
+    $(document).ready(function(){
+      $('.slider').slick({
+        autoplay: true,
+      });
+    });
+  }
 });
