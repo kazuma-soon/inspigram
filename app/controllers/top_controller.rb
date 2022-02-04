26b759054cff3875c8b431a86dcf6521bfd0d5d1
kazuma-soon <podradio2020@gmail.com>
2022-02-05 00:55:14 +0900

    Add: PCウィンドウに対応したスライドショーの追加#47

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,5 +2,7 @@ class TopController < ApplicationController
   skip_before_action :check_logged_in, only: :index
 
   def index
+    # randomとlimit使うか、
+    @boards = Board.find(Board.pluck(:id).shuffle[0..5])
   end
 end
---
+++ app/javascript/src/slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    centerMode: true,
+    slidesToShow: 3,
+    arrows: false,
+    autoplay: true,
+  });
+});
