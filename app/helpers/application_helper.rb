3bd35516f8eb7ea81533c98076c73ed9ef04da88
kazuma-soon <podradio2020@gmail.com>
2022-02-06 04:13:26 +0900

    Improve: タイトルを動的出力#51

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def liked_count_of(board)
     board.reactions.where(status: 'like').count
   end
+
+  def page_title(page_title = '')
+    base_title = 'Inspigram'
+
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
 end
+
+
