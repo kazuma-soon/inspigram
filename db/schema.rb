4177b51cff8141450ef9b9453e9c8c48cabea185
kazuma-soon <podradio2020@gmail.com>
2022-02-07 21:45:34 +0900

    Add: adminカラムとadminユーザーの設定#50

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_06_153418) do
+ActiveRecord::Schema.define(version: 2022_02_07_124220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2022_02_06_153418) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0, null: false
   end
 
   add_foreign_key "boards", "users"
