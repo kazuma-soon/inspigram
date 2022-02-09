bc1b3c941c6d80476e5492b4582965ebb9546ef0
kazuma-soon <podradio2020@gmail.com>
2022-02-10 05:13:53 +0900

    Fix: masonry.js修正

--- app/javascript/src/masonry.js
+++ app/javascript/src/masonry.js
@@ -3,10 +3,9 @@ $(function(){
 
   $container.imagesLoaded(function(){ //imagesLoadedを使用し、画像が読み込みまれた段階でMasonryの関数を実行させる
 
-    $('.js-masonry-container').masonry({
+    $container.masonry({
         itemSelector: '.js-masonry-item',
         columnWidth: '.js-masonry-sizer',
-        percentPosition: true
     });
   });
 });
