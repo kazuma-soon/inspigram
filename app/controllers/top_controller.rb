6c6d01e3da9ed25d1be4fcc4b7f0bec1c6e68233
kazuma-soon <podradio2020@gmail.com>
2022-02-21 07:08:07 +0900

    Add: ゲストのための説明を追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,8 +1,12 @@
 class TopController < ApplicationController
-  skip_before_action :check_logged_in, only: :index
+  skip_before_action :check_logged_in, only: %i[index guest_index]
 
   def index
     # randomとlimit使うか、人気ランキングをここで行う
     @boards = Board.find(Reaction.where(status: 'like').group(:board_id).order('count(board_id) desc').limit(6).pluck(:board_id))
+  end
+
+  def guest_index
+    @all_boards = Board.all
   end
 end
