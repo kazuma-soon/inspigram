958da2447c80829d871dd4b127f016e2fc2befdb
kazuma-soon <podradio2020@gmail.com>
2022-03-11 22:13:31 +0900

    Fix: mailer の削除

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,26 +1,3 @@
 class ContactsController < ApplicationController
-  skip_before_action :check_logged_in
-  
-  def new
-    @contact = Contact.new
-  end
-
-  def create
-    @contact = Contact.new(contact_params)
-    if @contact.save
-      ContactMailer.contact_mail(@contact).deliver
-      redirect_to thanks_contacts_path
-    else
-      render :new
-    end
-  end
-
-  def thanks
-  end
-
-  private
-
-  def contact_params
-    params.require(:contact).permit(:name, :email, :content)
-  end
+  def new ;end
 end
--- app/mailers/contact_mailer.rb
+++
@@ -1,6 +0,0 @@
-class ContactMailer < ApplicationMailer
-  def contact_mail(contact)
-    @contact = contact
-    mail to: 'virtialworld3601@gmail.com', subject: 'inspigramに関するお問い合わせ'
-  end
-end
