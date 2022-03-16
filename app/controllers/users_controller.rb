c775390b4c6c2c4666bbde7727cc67882b39e5b4
kazuma-soon <podradio2020@gmail.com>
2022-03-17 04:10:37 +0900

    Fix: user_controllerの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   def likes
-    @like_boards = []
+    # @like_boards = []
 
-    # 繰り返しがやばすぎる　joinを使う includeは違う　見直し has_many through
-    current_user.favorite_boards.each do |like_board|
-      @like_boards << Board.find(like_board[:board_id])
-    end
-    @like_boards
+    ## SQLのパフォーマンスがダメ has_many through source で改善
+    # current_user.favorite_boards.each do |like_board|
+    #   @like_boards << Board.find(like_board[:board_id])
+    # end
+    # @like_boards
+    
+    @like_boards = current_user.like_boards
   end
 end
--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -3,4 +3,6 @@ class Reaction < ApplicationRecord
   belongs_to :board
 
   enum status: { like: 0, dislike: 1 }
+
+  scope :likes, -> { where(status: 'like').order(updated_at: :desc) }
 end
