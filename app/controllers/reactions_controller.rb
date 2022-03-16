0c8a3c6ceb3ede9196e2a280f23630efba4b8aff
kazuma-soon <podradio2020@gmail.com>
2022-03-16 20:39:09 +0900

    Fix: レビュー会のコメント

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,5 +1,6 @@
 class ReactionsController < ApplicationController
   def create
+    # updateはinitializeと併用できる？
     reaction = Reaction.find_or_initialize_by(board_id: params[:board_id], user_id: current_user.id)
     reaction.update(
       status: params[:reaction]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def likes
     @like_boards = []
 
+    # 繰り返しがやばすぎる　joinを使う includeは違う　見直し has_many through
     current_user.favorite_boards.each do |like_board|
       @like_boards << Board.find(like_board[:board_id])
     end
