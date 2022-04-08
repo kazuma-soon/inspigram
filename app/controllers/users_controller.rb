52b25e084ac76f2317cd10cc42e2acce379442c7
kazuma-soon <podradio2020@gmail.com>
2022-04-08 19:20:46 +0900

    Delete: 不必要なコメントの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,5 @@
 class UsersController < ApplicationController
   def likes
-    # @like_boards = []
-
-    ## SQLのパフォーマンスがダメ has_many through source で改善
-    # current_user.favorite_boards.each do |like_board|
-    #   @like_boards << Board.find(like_board[:board_id])
-    # end
-    # @like_boards
-    
     @like_boards = current_user.like_boards
   end
 end
